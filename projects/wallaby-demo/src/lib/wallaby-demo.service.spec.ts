278fe133019dec689af1cc8dbcd48804febd79b7
Alexey Karpov <karpovad@yandex.ru>
2020-03-03 13:10:41 +0100

    Added app

--- projects/wallaby-demo/src/lib/wallaby-demo.service.spec.ts
+++ projects/wallaby-demo/src/lib/wallaby-demo.service.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { WallabyDemoService } from './wallaby-demo.service';
+import { WallabyDemoService } from './wallaby-demo.service'
 
 describe('WallabyDemoService', () => {
-  let service: WallabyDemoService;
+  let service: WallabyDemoService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WallabyDemoService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(WallabyDemoService)
+  })
 
+  // Try to change this to fit() or xit()
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+
+  it('', () => {
+    // Try to change to 6
+    expect(5).toEqual(5)
+  })
+})
